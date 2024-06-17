f6544e145e0f535495def0315f4370ef54d68cc6
arieltroian <arieltroian@outlook.com>
2024-06-17 13:44:09 -0300

    feat: add Java class file to explain boolean operators

---
+++ src/main/java/edu/ariel/operadoresbooleanos/OperadoresBooleanos.java
@@ -0,0 +1,15 @@
+package edu.ariel.operadoresbooleanos;
+
+public class OperadoresBooleanos {
+    public static void main(String[] args) {
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+
+        String mensagem1 = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        String mensagem2 = fazendoSol ? "está fazendo sol!" : "não está fazendo sol.";
+        System.out.println(mensagem1 + " e " + mensagem2);
+
+        String vamosAPraia = fimDeSemana && fazendoSol ? "Vamos à praia!" : "Não vamos à praia :(";
+        System.out.println("Então: " + vamosAPraia);
+    }
+}
